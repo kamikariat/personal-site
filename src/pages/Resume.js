05282a63b1f2faf72b46790b1db586d2916413c8
KevinIsMyName <wu.kevin.2520@gmail.com>
2021-08-10 22:42:19 -0400

    Fixed link to resume pdf

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Main from '../layouts/Main';
+import Main from "../layouts/Main";
 
-import Education from '../components/Resume/Education';
-import Experience from '../components/Resume/Experience';
+import Education from "../components/Resume/Education";
+import Experience from "../components/Resume/Experience";
 // import Skills from '../components/Resume/Skills';
 // import Courses from '../components/Resume/Courses';
-import Extracurriculars from '../components/Resume/Extracurriculars';
-import References from '../components/Resume/References';
+import Extracurriculars from "../components/Resume/Extracurriculars";
+import References from "../components/Resume/References";
 
 // import courses from '../data/resume/courses';
-import degrees from '../data/resume/degrees';
-import jobs from '../data/resume/jobs';
-import extras from '../data/resume/extras';
+import degrees from "../data/resume/degrees";
+import jobs from "../data/resume/jobs";
+import extras from "../data/resume/extras";
 // import { skills, categories } from '../data/resume/skills';
 
+import Pdf from "../data/resume/resume.pdf";
+
 const sections = [
-  'Education',
-  'Experience',
+  "Education",
+  "Experience",
   // 'Skills',
   // 'Courses',
   // 'References',
-  'Extracurriculars',
+  "Extracurriculars",
 ];
 
 const Resume = () => (
@@ -40,7 +42,11 @@ const Resume = () => (
               </h4>
             ))}
           </div>
-          <a href="https://www.google.com" target="_blank" rel="noreferrer">Click here for a much more readable resume</a>
+          <div className="App">
+            <a href={Pdf} target="_blank" rel="noreferrer">
+              Click here for a much more readable resume.
+            </a>
+          </div>
         </div>
       </header>
       <Education data={degrees} />
