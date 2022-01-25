16e4d21ccc196bf696085abd6c39dd314f563dd8
KevinIsMyName <wu.kevin.2520@gmail.com>
2022-01-25 11:44:10 -0500

    Update about and projects

--- src/data/about.md
+++ src/data/about.md
@@ -1,29 +1,29 @@
 # Intro
 
-I am a pre-junior (year 3/5) computer science student at [Drexel University](https://drexel.edu/), Philadelphia, PA. I have one year of junior experience at [Susquehanna International Group](https://sig.com/) and [Bentley Systems](https://www.bentley.com/) in both frontend and backend development.
+I am a junior (year 4/5) computer science student at [Drexel University](https://drexel.edu/), Philadelphia, PA. I have one year of junior experience across my time at [Susquehanna International Group](https://sig.com/) and [Bentley Systems](https://www.bentley.com/) in both frontend and backend development.
 
 I am a go-getter that enjoys problem solving and socializing with people. Most of all, I am excited to learn more about the world of technology and how it can connect people.
 
 # Currently
 
-I am finishing up my second co-op at [Bentley Systems](https://www.bentley.com/) and will begin classes in the fall. I will be embarking on my two tracks: artificial intelligence and computer vision. I am very excited to be back in person again!
+I just finished my second co-op at [Bentley Systems](https://www.bentley.com/) and am currently in classes. I will be embarking on my two tracks: artificial intelligence and computer vision. I am very relieved to be back in person again (but safety is always number one)!
 
 # I like
 
 - Chess
+- First-person-shooter video games (e.g. Counter-Strike)
 - Super Smash Bros. Ultimate
-- Pool / Billiards
+- Pool (not the swimming one)
 - Anime
 - Ice skating
 - Biking
 - Photography
 - Travelling
-- First-person-shooter video games
 
 # Fun facts
 
 - I learned to play chess all by myself and played nationally in the USCF from 2015-2017.
-- I type very fast. I have an average WPM of 125, and my highest WPM is 155.
+- I type very fast. I have an average WPM of 136, and my highest WPM is 163.
 - I attended my first hackathon in 2019, but never presented (we all chickened!).
 - I used to play the piano. I wish I continued playing because I find it fun now, but I definitely don't miss the practice sessions.
 - I have a lot of side projects that have never survived and live in the dark corner called _private repos_.
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,12 +1,20 @@
 // TODO Add a couple lines about each project
 const data = [
   {
+    title: 'Kwotes',
+    // image: '/images/projects/missing.jpg',
+    link: 'https://github.com/KevinIsMyName/kwotes',
+    date: '2022-01-01',
+    desc:
+      'A website that shows some of the more NSFW quotes my friends have said. Built with Node Express.js. Deployed with DigitalOcean.',
+  },
+  {
     title: 'Fake Profile',
     // image: '/images/projects/missing.jpg',
     link: 'https://github.com/kevinismyname/fake-profile',
     date: '2020-12-01',
     desc:
-      'A quick React app fake profile generator using external APIs to learn more about React',
+      'A fake profile generator to learn more about React.js. Built with React.js and external APIs.',
   },
   {
     title: 'CryptoSim',
@@ -14,7 +22,7 @@ const data = [
     link: "https://github.com/jzlotek/bc",
     date: '2020-12-01',
     desc:
-      'A Vue.js Golang Dockerized web app that simulates a proof-of-work blockchain',
+      'A proof-of-work blockchain simulator. Built with Vue.js, Golang, and Docker+Kubenetes.',
   },
   {
     title: 'Attack on Checkers',
@@ -22,7 +30,7 @@ const data = [
     link: 'https://github.com/lixinwei123/SE181',
     date: '2020-12-01',
     desc:
-      'A Ionic Angular Firebase web app to play checkers privately with your friends, Attack on Titan style.',
+      'An Attack-on-Titan-styled checkers game to play with friends online. Built with Ionic Angular and Google Firebase.',
   },
   {
     title: 'MCWebStart',
@@ -30,7 +38,7 @@ const data = [
     link: 'https://github.com/KevinIsMyName/MCWebStart',
     date: '2020-08-01',
     desc:
-      'A basic HTML/JS Python Flask website where players can view a local server\'s status and spin up the Minecraft server',
+      'A website where players can start up and view my Minecraft server\'s status. Built with Python Flask.',
   },
   {
     title: 'SimpliFi',
@@ -38,7 +46,7 @@ const data = [
     link: 'https://github.com/unitehenry/simply-finance',
     date: '2019-04-01',
     desc:
-      'A React.js Node.js web app that scans receipts and tracks budgeting via Tesseract OCR and NLP API, part of DragonHacks 2019',
+      'A budget-tracking receipt-scanning web app. Built with React.js, Node.js, Google Tesseract OCR and NLP API. Part of DragonHacks 2019',
   },
   {
     title: 'Balloono',
@@ -46,7 +54,7 @@ const data = [
     link: 'https://github.com/KevinIsMyName/Balloono',
     date: '2019-02-01',
     desc:
-      'A bomberman game created with HTML, JS, and CSS, part of HackTCNJ 2019',
+      'A bomberman game. Built with with HTML, JS, and CSS. Part of HackTCNJ 2019',
   },
 ];
 
