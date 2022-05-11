51fc77edcbba65ea0b2b8c61aa26fdb93620e7f4
KevinIsMyName <wu.kevin.2520@gmail.com>
2022-05-11 16:07:56 -0400

    Slight typo fix

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -28,7 +28,7 @@ const Index = () => (
       </p>
       <p>
         {" "}
-        Source available{" "}
+        Source code available{" "}
         <a href="https://github.com/kevinismyname/personal-site">here</a>.
       </p>
 
